7ddb999ec6789f0909fa76f25e4a9d54afe51842
Henrique Layber <henriquelayber@gmail.com>
2019-06-22 22:10:08 -0300

    AutoPush

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include "../headers/lista.h"
 #include "../headers/arquivos.h"
+#include <time.h>
 
 int main(int argc, char *argv[]){
-    if(argc <= 2){
+    if(argc <= 2 || atoi(argv[1]) < 1 ){
         printf("#Usagem do programa:\n#./LEIA_O_MAKEFILE [nPalavras] [arquivos..]\n#\tOnde:\n#nPalavras = numeros de palavras a aleatorias a ser pesquisada em cada estrutura\n#arquivos = Os arquivo que serao passados para o programa indexar, separados por espaço.\n");
         return 0;
     }
-
 
     tListaSent *l[argc - 2];    //os 2 primeiros são o nome do executável e o n de palavras
     char pal[NPAL]; //tamanho arbitrariamente grande
@@ -26,8 +26,23 @@ int main(int argc, char *argv[]){
 
         fecha_Arquivo(arquivo);
 
-        printf("========LISTA %d:========\n", i - 1);
-        print_Lista(l[i-2]);
+        //printf("========LISTA %d:========\n", i - 1);
+        //print_Lista(l[i-2]);
+    }
+
+    //sorteia as palavras a serem pesquisadas.
+    //escolhe primeiro a lista, e aí acessa um número aleatório na lista e trás somente a palavra
+    srand(time(NULL));
+    for(int i = 0; i < atoi(argv[1]); i++){
+        srand(rand());
+        tListaSent *ListaSelec = l[rand() % (argc - 2)];
+        srand(rand());
+        int indSelec = rand() % ListaSelec->qtd;
+        tLista *noSelec = ListaSelec->ini;
+        for(int y = 0; y < indSelec; y++)
+            noSelec = noSelec->prox;
+
+        printf("Palavra selecionada: %s\n", noSelec->palavra);
     }
 
     for(int i = 0; i < argc - 2; i++)
