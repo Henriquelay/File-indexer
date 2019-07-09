245dd1eafd0294e41003c9fec5dbcc6f05afd333
HenriqueLay <henriquelayber@gmail.com>
2019-07-09 16:21:05 -0300

    bin pronta avl comecei a mexer

--- headers/ArvoreBinaria.h
+++ headers/ArvoreBinaria.h
@@ -1,26 +1,24 @@
 #ifndef _ARVBIN_H_
 #define _ARVBIN_H_
 
+#include "arquivos.h"
+#include "base.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "arquivos.h"
-#include "base.h"
 
 //----STRUCTS--------
 typedef struct NO tNo;
 typedef tNo* ArvBin;
 typedef struct NO{ 
-    char* info; 
+    tPalavra *palavra;
     ArvBin esq, dir;
-    int ocorrencias;
-    tIndiceLista *indices;
 } tNo;
 
 //----FUNCOES-------
 ArvBin* cria_ArvBin();
-char insere_ArvBin(ArvBin* raiz, char* palavra, int byte);
+char insere_ArvBin(ArvBin* raiz, char* palavra, int byte, char arq);
 void destroi_ArvBin(ArvBin *raiz);
 char consulta_ArvBin(ArvBin *raiz, char* palavra);
 void emOrdem(ArvBin *raiz);
